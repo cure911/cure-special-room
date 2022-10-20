34035b08182a774105b0262b20892ebfe8f65f28
Mirac <115193668+cure911@users.noreply.github.com>
2022-10-19 20:21:23 -0400

    Add files via upload

---
+++ traplife-identitiy/script.js
@@ -0,0 +1,5 @@
+var signature = document.querySelector(".signature");
+var namE = document.querySelector(".name");
+var surname = document.querySelector(".surname");
+
+signature.innerHTML = namE.innerHTML + surname.innerHTML
